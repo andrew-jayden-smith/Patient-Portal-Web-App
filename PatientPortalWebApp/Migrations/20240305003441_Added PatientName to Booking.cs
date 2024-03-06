9450603759ee061349a0cb679b078cb5632e285f
Drew <smithdrew867@gmail.com>
2024-03-05 23:09:48 -0500

    Added database patients

---
+++ PatientPortalWebApp/Migrations/20240305003441_Added PatientName to Booking.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PatientPortalWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPatientNametoBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PatientName",
+                table: "Bookings",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PatientName",
+                table: "Bookings");
+        }
+    }
+}
